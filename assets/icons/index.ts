b8ed8973d933894f314b9fff89ccf1c55703a493
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-11-12 01:38:24 +0530

    add some blog elements

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,4 +1,5 @@
 import BehanceIcon from "./behance.svg";
 import GithubIcon from "./github.svg";
+import LikesIcon from "./favorite_border_black_24dp.svg";
 
-export { BehanceIcon, GithubIcon };
+export { BehanceIcon, GithubIcon, LikesIcon };
--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,18 +1,28 @@
 import { NO_OF_VARIANTS } from "../app.constants";
-import { Blog } from "../graphql-src/blog-api";
+import { LikesIcon } from "../assets/icons";
+import { Blog as BlogType } from "../graphql-src/blog-api";
 import { cJoin } from "../utils";
 import styles from "./Blog.module.scss";
 
 interface Props {
-  obj: Blog;
+  obj: BlogType;
   index: number;
 }
 const Blog = ({ obj, index }: Props) => {
   const variantNumber = (index % NO_OF_VARIANTS) + 1;
+  console.log(obj.updatedAt);
   return (
     <button className={cJoin(styles.container, `variant${variantNumber}`)}>
       <div className={styles.title}>{obj.title}</div>
       <div className={styles.summary}>{obj.summary}</div>
+      <div className={styles.header}>
+        <div className={styles.lastUpdatedPart}>
+          Last Updated <span>{new Date(obj.updatedAt).toDateString()}</span>
+        </div>
+        <div className={styles.likesPart}>
+          <LikesIcon /> <span className={styles.likes}>{obj.likes}</span>
+        </div>
+      </div>
     </button>
   );
 };
